71e561d45e5ac849fdb20ba33fc9ab139f99d28c
ssmarin23 <146219180+ssmarin23@users.noreply.github.com>
2024-05-13 11:19:33 -0500

    creacion controladores y vistas

---
+++ CONNOTI_PROYECTO/Controllers/GeneroController.cs
@@ -0,0 +1,54 @@
+﻿using Capa_Datos;
+using Capa_Modelo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CONNOTI_PROYECTO.Controllers
+{
+    public class GeneroController : Controller
+    {
+        // GET: Categoria
+        public ActionResult Crear()
+        {
+            return View();
+        }
+
+
+        public JsonResult Obtener()
+        {
+            List<Genero> lista = CD_Genero.Instancia.ObtenerGenero();
+            return Json(new { data = lista }, JsonRequestBehavior.AllowGet);
+        }
+
+
+        [HttpPost]
+        public JsonResult Guardar(Genero objeto)
+        {
+            bool respuesta = false;
+
+            if (objeto.IdGenero == 0)
+            {
+
+                respuesta = CD_Genero.Instancia.RegistrarGenero(objeto);
+            }
+            else
+            {
+                respuesta = CD_Genero.Instancia.ModificarGenero(objeto);
+            }
+
+
+            return Json(new { resultado = respuesta }, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpGet]
+        public JsonResult Eliminar(int id = 0)
+        {
+            bool respuesta = CD_Genero.Instancia.EliminarGenero(id);
+
+            return Json(new { resultado = respuesta }, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
